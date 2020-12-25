360eea51830c562da10b80256ebd677252c77cf9
llc <touldeguiacranmer@gmail.com>
2020-12-25 21:35:38 +0000

    Added missing method.

--- GO-devsecops-pipeline.go
+++ GO-devsecops-pipeline.go
@@ -11,6 +11,17 @@ type Server struct {
 	once sync.Once
 }
 
+func (a *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.once.Do(func() {
+		a.mux = http.NewServeMux()
+		a.mux.HandleFunc("/", a.home)
+		a.mux.HandleFunc("/login", a.login)
+		a.mux.HandleFunc("/admin", cookieAuthMw(a.admin))
+		a.mux.HandleFunc("/header-admin", headerAuthMw(a.admin))
+	})
+	a.mux.ServeHTTP(w, r)
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, DevSecOps!</h1>")
 }
