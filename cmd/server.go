3294ef74c5792fa8efa295d80d72b474208ccbc9
llc <touldeguiacranmer@gmail.com>
2020-12-25 21:41:46 +0000

    Handled error per linter demands.

--- cmd/server.go
+++ cmd/server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	hi "github.com/llcranmer/GO-devsecops-pipeline"
 )
 
 func main() {
-	http.ListenAndServe(":3000", &hi.Server{})
+	err := http.ListenAndServe(":3000", &hi.Server{})
+	if err != nil {
+		log.Fatal("Server is not working.")
+	}
 }
