9654b53676035e543c0fa20fa31f9d34e232892a
llc <touldeguiacranmer@gmail.com>
2020-12-30 08:00:30 +0000

    Reverted to an alpine based docker image.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Hi from alpine</h1>")
 }
 
 func main() {
