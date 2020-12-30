de854a20c79388a3c0de89efecaff4146d985c2d
llc <touldeguiacranmer@gmail.com>
2020-12-30 07:57:23 +0000

    Added correct steps.

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
