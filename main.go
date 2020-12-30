a0cab2bd4ce09a135b43de50e3ec5553e2d771a9
llc <touldeguiacranmer@gmail.com>
2020-12-30 01:57:23 +0000

    Added linting error.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -11,5 +12,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("Server is not working.")
+	}
 }
