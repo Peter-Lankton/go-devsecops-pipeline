e0f0e30a306ede486c17ac750d57c1d6cd59abea
llc <touldeguiacranmer@gmail.com>
2020-12-30 03:07:58 +0000

    Changed the port.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	err := http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal("Server is not working.")
 	}
